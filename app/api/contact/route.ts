3666ec2643c67e7fe58ebbd765015a84ea8c67dd
Lance Ziegler <lance.ziegler@hotmail.com>
2023-09-29 18:39:15 -0700

    re added route for error

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -40,52 +40,52 @@
 // //   }
 // // }
 
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import nodemailer from 'nodemailer';
+import { NextApiRequest, NextApiResponse } from 'next';
+import nodemailer from 'nodemailer';
 
-// // export const config = {
-// //   api: {
-// //     bodyParser: true,
-// //   },
-// // };
+// export const config = {
+//   api: {
+//     bodyParser: true,
+//   },
+// };
 
-// export const runtime = 'nodejs';
+export const runtime = 'nodejs';
 
-// export async function POST(req: Request) {
-//   try {
-//     const res = await req.json();
+export async function POST(req: Request) {
+  try {
+    const res = await req.json();
 
-//     // Create a Nodemailer transporter with your email provider settings
-//     const transporter = nodemailer.createTransport({
-//       // Configure your email provider here
-//       service: 'Gmail',
-//       auth: {
-//         user: process.env.email,
-//         pass: process.env.password,
-//       },
-//     });
+    // Create a Nodemailer transporter with your email provider settings
+    const transporter = nodemailer.createTransport({
+      // Configure your email provider here
+      service: 'Gmail',
+      auth: {
+        user: process.env.email,
+        pass: process.env.password,
+      },
+    });
 
-//     // Email content
-//     const mailOptions = {
-//       from: process.env.email,
-//       to: process.env.email, // Send the email to yourself
-//       subject: 'New Contact Form Submission',
-//       text: `Name: ${res.name}\nEmail: ${res.email}\nMessage: ${res.message}`,
-//     };
+    // Email content
+    const mailOptions = {
+      from: process.env.email,
+      to: process.env.email, // Send the email to yourself
+      subject: 'New Contact Form Submission',
+      text: `Name: ${res.name}\nEmail: ${res.email}\nMessage: ${res.message}`,
+    };
 
-//     // Send the email
-//     await transporter.sendMail(mailOptions);
+    // Send the email
+    await transporter.sendMail(mailOptions);
 
-//     // res.status(200).json({ message: 'Email sent successfully' });
-//     return new Response(
-//       JSON.stringify({ message: 'Email sent successfully' }),
-//       { status: 200 }
-//     );
-//   } catch (error) {
-//     console.error(error);
-//     // res.status(500).json({ message: 'Error sending email' });
-//     return new Response(JSON.stringify({ message: 'Error sending email' }), {
-//       status: 200,
-//     });
-//   }
-// }
+    res.status(200).json({ message: 'Email sent successfully' });
+    return new Response(
+      JSON.stringify({ message: 'Email sent successfully' }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    // res.status(500).json({ message: 'Error sending email' });
+    return new Response(JSON.stringify({ message: 'Error sending email' }), {
+      status: 200,
+    });
+  }
+}
